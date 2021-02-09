92562fb6f2034affcd4e4d615bc956ae791f80a8
Matthew Rennie <36052246+Matthew-Rennie@users.noreply.github.com>
2021-02-09 05:09:23 +0000

    smooth movement of generic melee attacker
    i know thats not what this branch is for but i dont care

--- DES310 Project 1/Assets/Scripts/AI/GenericMeleeAttacker.cs
+++ DES310 Project 1/Assets/Scripts/AI/GenericMeleeAttacker.cs
@@ -1,80 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Threading;
+using UnityEngine;
+
+// TODO @matthew - can attack after moving
 
 namespace AI
 {
     public class GenericMeleeAttacker : AIBaseBehavior
     {
+        public int movement_distance = 1;
+        public float speed = 1f;
 
-        PathFinder pathFinder;
-        Mutex mutex = new Mutex();
-        int paths_finished = 0;
-        int path_count = 0;
-        List<Vector3[]> path_list = new List<Vector3[]>();
-        AICore ai_core;
+        private PathFinder pathFinder;
+        private Mutex mutex = new Mutex(); // coroutines are still run single threaded, this may be overkill
+        private int paths_finished = 0;
+        private int path_count = 0;
+        private List<Vector3[]> path_list = new List<Vector3[]>();
+        private AICore ai_core;
 
-        public override void run(AICore aiCore) 
+        // entry point, starts up coroutines and sets the turn_completed flag
+        public override void run(AICore aiCore)
         {
             ai_core = aiCore;
             pathFinder = aiCore.pathfinder;
             paths_finished = 0;
             path_count = 0;
-
-
+            turn_completed = false;
             BeginPathfindingCoroutines(aiCore);
-
         }
 
-        void OnPathFound(Vector3[] newPath, bool pathSuccess)
+        // run by the pathfinding system when a path is found or its determined impossible
+        private void OnPathFound(Vector3[] newPath, bool pathSuccess)
         {
-            mutex.WaitOne();
-            
+            mutex.WaitOne();// blocks until all paths have been added to the queue
+
             if (pathSuccess)
             {
                 path_list.Add(newPath);
-                Debug.Log("path found");
-            }
-            else
-            {
-                Debug.Log("path failed");
             }
             paths_finished++;
 
-
-            if(paths_finished == path_count)
+            // only run after the final path has been found
+            if (paths_finished == path_count)
             {
                 Vector3[] closest_path = FindClosestPartyMemberPath(path_list);
 
                 if (IsEnemyInAttackRange(closest_path))
                 {
+                    // target is in ajacent tile
                     GameObject enemy = GetPartyMemberObjectFromPath(closest_path, ai_core);
                     attack(enemy);
                 }
                 else
                 {
-                    move(closest_path);
+                    // move towards enemy
+                    StartCoroutine(move(closest_path));
                 }
             }
 
-
             mutex.ReleaseMutex();
+            turn_completed = true;
         }
 
-        Vector3[] FindClosestPartyMemberPath(List<Vector3[]> list) 
+        private Vector3[] FindClosestPartyMemberPath(List<Vector3[]> list)
         {
             Vector3[] closest = null;
-            
-            foreach(Vector3[] path in list)
+
+            foreach (Vector3[] path in list)
             {
-                if(closest == null)
+                if (closest == null)
                 {
                     closest = path;
                     continue;
                 }
 
-                if(path.Length < closest.Length)
+                if (path.Length < closest.Length)
                 {
                     closest = path;
                 }
@@ -83,15 +84,21 @@ namespace AI
             return closest;
         }
 
-        GameObject GetPartyMemberObjectFromPath(Vector3[] path, AICore aiCore)
+        // get a reference to the combatant at the end of the path
+        // this could be done by storing the object when requesting the path but that requires a refactor of other system
+        // this will be good enough
+        private GameObject GetPartyMemberObjectFromPath(Vector3[] path, AICore aiCore)
         {
             if (path == null)
                 return null;
 
+            if (path.Length == 0)
+                return null;
+
             Vector3 pos = path[path.Length - 1];
-            foreach(GameObject entity in aiCore.party_list)
+            foreach (GameObject entity in aiCore.party_list)
             {
-                if(entity.GetComponent<Transform>().position == pos)
+                if (entity.GetComponent<Transform>().position == pos)
                 {
                     return entity;
                 }
@@ -99,7 +106,8 @@ namespace AI
             return null;
         }
 
-        bool IsEnemyInAttackRange(Vector3[] path) 
+        // checks if a enemy is in a attack range of the combatants current location
+        private bool IsEnemyInAttackRange(Vector3[] path)
         {
             if (path == null)
                 return false;
@@ -107,10 +115,23 @@ namespace AI
             if (path.Length > 1)
                 return false;
             else
-                return true;        
+                return true;
         }
 
-        void BeginPathfindingCoroutines(AICore aiCore)
+        // checks if an enemy is in attack rage on a given tile on the path
+        private bool IsEnemyInAttackRange(Vector3[] path, int next_position)
+        {
+            if (path == null)
+                return false;
+
+            if (path.Length - next_position > 1)
+                return false;
+            else
+                return true;
+        }
+
+        // sets up the coroutines that calculate the path
+        private void BeginPathfindingCoroutines(AICore aiCore)
         {
             if (aiCore == null)
                 return;
@@ -133,20 +154,44 @@ namespace AI
             }
         }
 
-        void move(Vector3[] path)
+        // moves the ai along the path
+        private IEnumerator move(Vector3[] path)
         {
-            GetComponent<Transform>().position = path[0];
+            int targetIndex = 0;
+            Vector3 currentWaypoint = path[targetIndex];
+
+            while (true)
+            {
+                if (transform.position == currentWaypoint)
+                {
+                    targetIndex++;
+                    if (targetIndex >= path.Length)
+                    {
+                        yield break;
+                    }
+                    if (targetIndex == movement_distance)
+                    {
+                        yield break;
+                    }
+                    if (IsEnemyInAttackRange(path, targetIndex))
+                    {
+                        yield break;
+                    }
+
+                    currentWaypoint = path[targetIndex];
+                }
+
+                transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+
+                yield return null;
+            }
         }
 
-        void attack(GameObject enemy) 
+        // deal damage to the enemy
+        // TODO @matthew - this should use the combatants abilities insted of just dealing damage
+        private void attack(GameObject enemy)
         {
             enemy.GetComponent<Combatant>().do_damage(1, Combatant.DamageType.NORMAL);
-        } 
-
+        }
     }
 }
-
-
-
-
-
