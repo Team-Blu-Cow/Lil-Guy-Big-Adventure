eb1d8ccdadb92124142d3141dd872a4a426ad44d
Adam Emslie <44575716+yaboiadam00@users.noreply.github.com>
2021-02-22 14:12:37 +0000

    Menu Animation
     - added menu animations

--- DES310 Project 1/Assets/Scripts/UI/UI.cs
+++ DES310 Project 1/Assets/Scripts/UI/UI.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    public GameObject buttons;
+    public Animator buttons;
+    public Animator title;
+
     // Start is called before the first frame update
 
     private void Start()
     {
+        buttons.SetTrigger("UI Trigger Start");
+        title.SetTrigger("UI Trigger Title");
     }
 
     // Update is called once per frame
